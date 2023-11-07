c9ebd2ccaa7d13aaa928c4a49d028cae87666aa8
i <101498370+isaadev@users.noreply.github.com>
2023-11-07 15:12:25 -0500

    api call support

--- backend/app.js
+++ backend/app.js
@@ -1,19 +1,38 @@
 const express = require("express");
 const app = express();
-const request = require("request");
+const passport = require("passport");
+const mongoose = require("mongoose");
+const axios = require("axios");
+const dotenv = require("dotenv");
+require("dotenv").config();
 const port = 3000;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-/* 
-* this is a backend of a decoupled project where we take a string from the front end
-* and send it to the backend to make an api call then the json object back to the front end
-*
-*/
 // API call using the string from front end and make call to calorieninjas api
-
+app.get("/api/:food", async (req, res) => {
+  // get the string from the front end
+  const food = req.params.food;
+  // make the api call
+  try {
+    const response = await axios.get(
+      `https://api.calorieninjas.com/v1/nutrition?query=${food}`,
+      {
+        // API key from .env file
+        headers: {
+          "X-Api-Key": process.env.API_KEY,
+        },
+      }
+    );
+    // send the json object back to the front end
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred while fetching data");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
