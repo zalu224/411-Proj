95ebabd68f13db69517f0546d090e5352355d2e7
victor <victor.verma@icloud.com>
2023-11-10 15:12:18 -0500

    added response dispaly to home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,12 +3,14 @@ import axios from "axios";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { Link } from "react-router-dom";
 
+import NutritionResponse from "../../components/NutritionResponse";
+
 import "./Home.css";
 
 const Home = () => {
   const [query, setQuery] = useState(" ");
   const [loading, setLoading] = useState(false);
-  const [isResponse, setIsResponse] = useState(false);
+  const [isResponse, setIsResponse] = useState(true);
 
   // store this in database
   const creatorLinks = {
@@ -21,18 +23,56 @@ const Home = () => {
 
   const handleQuery = () => {
     setLoading(true);
+    if (query.trim() === "") {
+      console.log("query is empty");
+      setLoading(false);
+      return;
+    }
     axios
       .get("get request for calorie api results")
       .then((response) => {
         /* parse api response data */
         console.log(response);
-        setIsResponse(true);
+        setIsResponse(false);
         setLoading(false);
       })
       .catch((error) => {
         console.log(error);
         setLoading(false);
       });
+  };
+
+  const sampleResponse = {
+    items: [
+      {
+        sugar_g: 13.3,
+        fiber_g: 4,
+        serving_size_g: 283.495,
+        sodium_mg: 8,
+        name: "onion",
+        potassium_mg: 99,
+        fat_saturated_g: 0.1,
+        fat_total_g: 0.5,
+        calories: 126.7,
+        cholesterol_mg: 0,
+        protein_g: 3.9,
+        carbohydrates_total_g: 28.6,
+      },
+      {
+        sugar_g: 2.6,
+        fiber_g: 1.2,
+        serving_size_g: 100,
+        sodium_mg: 4,
+        name: "tomato",
+        potassium_mg: 23,
+        fat_saturated_g: 0,
+        fat_total_g: 0.2,
+        calories: 18.2,
+        cholesterol_mg: 0,
+        protein_g: 0.9,
+        carbohydrates_total_g: 3.9,
+      },
+    ],
   };
 
   return (
@@ -45,24 +85,25 @@ const Home = () => {
         </div>
         <h1 className="home-title">Application Title</h1>
       </div>
-      {isResponse ? (
-        "placeholder"
-      ) : loading ? (
-        "placeholder"
-      ) : (
-        <div className="search-bar">
-          <h2 className="search-bar-title">What did you eat?</h2>
-          <input
-            className="search-bar-input"
-            type="text"
-            value={query}
-            onChange={(i) => setQuery(i.target.value)}
-          />
-          <button className="search-bar-button" onClick={handleQuery}>
-            Analyze my nutrition!
-          </button>
-        </div>
-      )}
+      <div className="search-bar">
+        <h2 className="search-bar-title">What did you eat?</h2>
+        <input
+          className="search-bar-input"
+          type="text"
+          value={query}
+          onChange={(i) => setQuery(i.target.value)}
+        />
+        <button className="search-bar-button" onClick={handleQuery}>
+          Analyze my nutrition!
+        </button>
+        {loading ? (
+          "loading"
+        ) : isResponse ? (
+          <NutritionResponse response={sampleResponse.items} />
+        ) : (
+          "no response"
+        )}
+      </div>
       <div className="home-footer">
         Created by{" "}
         <a
