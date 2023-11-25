8d06128cd4c9f9a62364bea9d06f02932763919f
i <101498370+isaadev@users.noreply.github.com>
2023-11-25 12:32:23 -0500

    Added backend folder

---
+++ backend/app.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const app = express();
+const session = require("express-session");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+require("dotenv").config();
+const port = 3000;
+
+// routes/middleware
+const { isAuthenticated, ensureAuthenticated } = require("./middleware");
+const authRoutes = require("./routes/authRoutes");
+const passport = require('./passportConfig');
+const apiRoutes = require("./routes/apiRoutes");
+
+
+// connect to mongoDB
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB Connected ✓"))
+  .catch((err) => console.error("MongoDB connection error:", err));
+
+// not sure if i still need this?
+const cors = require("cors");
+app.use(cors());
+
+// cookie parser
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+// Use the express-session middleware
+app.use(
+  session({
+    secret: process.env.SESSION_KEY, // Replace with your secret key
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+// Initialize Passport and restore authentication state, if any, from the session
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.get("/", ensureAuthenticated, (req, res) => {
+  res.send("Welcome to our program!");
+});
+
+// auth routes (google)
+app.use("/auth", authRoutes);
+// api routes
+app.use("/api", apiRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port} ✓`);
+});
---
+++ backend/passportConfig.js
@@ -0,0 +1,42 @@
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const User = require("./models/User"); // Adjust path as needed
+
+// JWT middleware, verifying token passed from front end
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "/auth/google/callback",
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const condition = { googleId: profile.id };
+        const userData = {
+          googleId: profile.id,
+          searchHistory: [],
+        };
+        const user = await User.findOrCreate(condition, userData);
+        done(null, user);
+      } catch (error) {
+        done(error);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error);
+  }
+});
+
+module.exports = passport;
