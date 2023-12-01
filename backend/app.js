d9f38418cd50b0b97a6847c655b6499a3244430e
EPmaple <tcen17@bu.edu>
2023-12-01 15:32:17 -0500

    fixed google oauth problem

--- backend/app.js
+++ backend/app.js
@@ -5,13 +5,14 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 require("dotenv").config();
 const port = 3000;
+const cors = require("cors");
 
 // routes/middleware
 const { isAuthenticated, ensureAuthenticated } = require("./middleware");
 const authRoutes = require("./routes/authRoutes");
 const passport = require('./passportConfig');
 const apiRoutes = require("./routes/apiRoutes");
-
+const jwt = require('jsonwebtoken'); // for generating JWT tokens
 
 // connect to mongoDB
 mongoose
@@ -19,15 +20,21 @@ mongoose
   .then(() => console.log("MongoDB Connected ✓"))
   .catch((err) => console.error("MongoDB connection error:", err));
 
-// not sure if i still need this?
-const cors = require("cors");
-app.use(cors());
+// cors setup
+const corsOptions = {
+  origin: 'http://localhost:5173', // the frontend port
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // required for cookies, authorization headers, etc.
+};
+
+app.use(cors(corsOptions));
 
 // cookie parser
 const cookieParser = require("cookie-parser");
 app.use(cookieParser());
 
 // Use the express-session middleware
+// for session management, to maintain info for a user across multiple requests
 app.use(
   session({
     secret: process.env.SESSION_KEY, // Replace with your secret key
@@ -36,9 +43,28 @@ app.use(
   })
 );
 
+// for parsing JSON payloads in incoming requests
+// populating req.body with the JSON data for easier access
+app.use(express.json());
+
 // Initialize Passport and restore authentication state, if any, from the session
 app.use(passport.initialize());
 app.use(passport.session());
+
+// Handle Google OAuth login
+app.post('/google-login', async (req, res) => {
+  try {
+    console.log('Request:', req);
+    console.log('Request Body:', req.body);
+    const { token } = req.body;
+
+    // Assuming you want to echo back the received token
+    res.status(200).json({ token: token });
+  } catch (error) {
+    console.error('Error during Google OAuth login:', error);
+    res.status(500).json({ error: 'An error occurred during login' });
+  }
+});
 
 
 app.get("/", ensureAuthenticated, (req, res) => {
@@ -49,6 +75,12 @@ app.get("/", ensureAuthenticated, (req, res) => {
 app.use("/auth", authRoutes);
 // api routes
 app.use("/api", apiRoutes);
+
+// route to check if server is accessible
+// access http://localhost:3000/test in browser
+app.get("/test", (req, res) => {
+  res.send("Server is working!");
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port} ✓`);
